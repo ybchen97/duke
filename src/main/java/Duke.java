406e882474acdc23fcaa49d122cccd5a0e77bbb4
yuanbo <yuanbo.chen97@gmail.com>
2019-08-21 23:51:26 +0800

    first commit

--- src/main/java/Duke.java
+++ src/main/java/Duke.java
@@ -1,4 +1,27 @@
+import java.util.Scanner;
+
 public class Duke {
+
+    private String hLine = "    ____________________________________________________________";
+
+    public void answer(String input) {
+        System.out.println(this.hLine);
+        System.out.println("    " + input);
+        System.out.println(this.hLine);
+    }
+
+    public void greeting() {
+        this.answer("Hello! I'm Duke\n    What can I do for you?");
+    }
+
+    public void farewell() {
+        this.answer("Bye. Hope to see you again soon!");
+    }
+
+    public void echo(String input) {
+        this.answer(input);
+    }
+
     public static void main(String[] args) {
         String logo = " ____        _        \n"
                 + "|  _ \\ _   _| | _____ \n"
@@ -6,5 +29,24 @@ public class Duke {
                 + "| |_| | |_| |   <  __/\n"
                 + "|____/ \\__,_|_|\\_\\___|\n";
         System.out.println("Hello from\n" + logo);
+
+        Duke duke = new Duke();
+
+        // Greeting from Duke
+        duke.greeting();
+
+        // Taking in user input
+        Scanner sc = new Scanner(System.in);
+        String input = sc.nextLine();
+
+        // Echo
+        while (!input.equals("bye")) {
+            duke.echo(input);
+            input = sc.nextLine();
+        }
+
+        duke.farewell();
+        sc.close();
+
     }
 }
